1726f5e22ebb21596d3abf36da3b2cd71aa1a121
suraj28j <suraj28j@gmail.com>
2023-08-29 18:07:04 +0530

    primitive data type

---
+++ DataType.js
@@ -0,0 +1,19 @@
+"use strict" // Treat all JS code as newer version
+
+//alert( 3 + 3) // bcoz we are using node JS not Browser
+
+
+// Primitive Data Type // 
+let name = "suraj" 
+let lname = 'kumar' 
+let age = 33  // 2 to power of 53
+let marks = 70.9
+let bigInteger = 198907281992122320130711
+let isLoggedIn = true
+let job // Undefine 
+let salary = null // Null - it represents a non-existent or a invalid value 
+let symbol1 = Symbol('Suraj');
+//typeof
+console.log(typeof (job)); //  undefine
+console.log(typeof (salary)); // object 
+console.log(typeof (symbol1)); //  symbol
