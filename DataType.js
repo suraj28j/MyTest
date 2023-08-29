591d82aa3fa0155ba901d10e9ec25082e64bd3a4
suraj28j <suraj28j@gmail.com>
2023-08-29 21:16:59 +0530

    type conversion

--- DataType.js
+++ DataType.js
@@ -3,7 +3,7 @@
 //alert( 3 + 3) // bcoz we are using node JS not Browser
 
 
-// Primitive Data Type // 
+// ---Primitive Data Type--- // // 
 let name = "suraj"
 let lname = 'kumar'
 let age = 33  // 2 to power of 53
@@ -21,7 +21,7 @@ console.log(typeof (symbol1)); //  symbol
 //Note - Primitive data types can store only a single value. To store multiple and complex values, non-primitive data types are used//
 
 
-// Non Primitive Data Type //
+// // ---Non Primitive Data Type--- // //
 // (1) Object
 // (2) Array
 
@@ -46,3 +46,61 @@ let Roll_No1 = {
 
 let Roll_No2 = ["Ravi", 29, true, 76];
 
+
+
+// ---Data Type Conversion--- //
+
+// type conversion is the process of converting data of one type to another. //
+// (1) Implicit Conversion - automatic type conversion
+// (2) Explicit Conversion - manual type conversion
+
+// (1) Implicit Conversion Examples
+
+// Example 1
+let num1 = 3;
+let num2 = "3";
+console.log(num1 + num2); // 33
+
+// Example 2
+let num3 = 24;
+let num4 = "Hello";
+console.log(num1 + num2); // 24Hello
+
+// Example 3
+let num5 = 3;
+let num6 = "3";
+console.log(num5 - num6); // return 0  since num6 (string type) is converted to a number type
+
+// Example 4
+let num7 = 3;
+let num8 = "Hello";
+let numR = num7 - num8;
+console.log(numR); // NaN 
+console.log(typeof (numR)); // data type is a number
+
+// Example 5
+let num9 = 4 - true;
+console.log(num9); // 3
+
+let num10 = 4 + false;
+console.log(num10);
+
+let num11 = 4 + undefined;
+console.log(num11);  // NaN
+
+let num12 = 4 - undefined;
+console.log(num12);  // NaN
+
+let num13 = true + undefined;
+console.log(num13);  // NaN
+
+let num14 = null + undefined;
+console.log(num14);  // NaN
+
+
+
+// (2) Explicit Conversion Examples
+
+// Example (1)
+let num15 = Number("123");
+console.log(num15);
