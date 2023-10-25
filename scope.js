4acbd031f35e83b9b3ac06aa0d55267ae3852b31
suraj28j <suraj28j@gmail.com>
2023-10-25 22:46:59 +0530

    scope

--- scope.js
+++ scope.js
@@ -21,4 +21,33 @@ function globalscope(){
     console.log(l1);
     console.log(c1);
 }
-globalscope();
+globalscope();  // All variables can be accessed anywhere since they are declared in the global scope
+
+
+
+
+// Function Scope //
+
+function funscope(){
+    var v2 = 30;
+    let l2 = 40;
+    const c2 = 50;
+}
+console.log(v2);  // Throws an error
+console.log(l2);  // Throws an error
+console.log(c2);  // Throws an error
+
+
+
+
+// Block Scope //
+
+{
+    var v3 = 100;
+    let l3 = 110;
+    const c3 = 120;
+}
+
+console.log(v3); // Output is 100
+console.log(l3); // Throws an error
+console.log(c3); // Throws an error
