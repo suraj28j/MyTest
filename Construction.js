7c9313d484befa65b56998d3044283884477b609
suraj28j <suraj28j@gmail.com>
2023-10-25 22:48:56 +0530

    file name changed

---
+++ Construction.js
@@ -0,0 +1,13 @@
+// Construction Function //
+
+const User = function (name, id, isLoggedIn) {
+    this.name = name; // in coding practice function veriable and function parameters must be same
+    this.id = id;
+    this.isLoggedIn = isLoggedIn;
+    return this
+}
+
+let user1 = User("Suraj", 101, true); // 'new' keyword call a construction function
+let user2 = User("Mohit", 20, false); // user2 values over wright 'User' 
+//console.log(user1);
+console.log(user2);
