d4d3e8be14eaba6ce38740de435e0758444c3d6d
suraj28j <suraj28j@gmail.com>
2023-10-26 21:37:07 +0530

    update

--- Construction.js
+++ Construction.js
@@ -7,7 +7,7 @@ const User = function (name, id, isLoggedIn) {
     return this
 }
 
-let user1 = User("Suraj", 101, true); // 'new' keyword call a construction function
-let user2 = User("Mohit", 20, false); // user2 values over wright 'User' 
+let user1 = new User("Suraj", 101, true); // 'new' keyword call a construction function
+let user2 = new User("Mohit", 20, false); // user2 values over wright 'User' 
 //console.log(user1);
 console.log(user2);
